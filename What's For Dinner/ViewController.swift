45c5a9cfed55545d9f72fd8d52e6b80b7b5429a3
Jessica Olivieri <jessicaolivieri@jessicas-macbook-pro.local>
2018-09-26 14:43:37 -0400

    1.1 What's for dinner

--- What's For Dinner/ViewController.swift
+++ What's For Dinner/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var dinnerImage: UIImageView!
+	@IBOutlet weak var dinnerLabel: UILabel!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-
+	@IBAction func dinnerButtonPressed(_ sender: UIButton) {
+		// declare array (could also place outside of function) -- better in this s
+		// put outside if you will be using it again later in the app
+		let choicesArray = ["Burgers", "Burritos", "Cobb Salad", "Pepperoni Pizza", "Sushi"]
+		
+		var selectedMeal = choicesArray.randomElement()!
+		while selectedMeal == dinnerLabel.text {
+			selectedMeal = choicesArray.randomElement()!
+		}
+		
+		dinnerLabel.text = selectedMeal
+		dinnerImage.image = UIImage(named: selectedMeal)
+	}
+	
 }
 
